306712cb2a090cf74ce28d53e84f333caec23130
Charles Provencher <cprovencher@mesosphere.com>
2020-01-08 14:52:05 -0800

    hello

--- main.go
+++ main.go
@@ -1,5 +1,5 @@
 /**
-asfd * Copyright 2017 Google Inc.
+ddasfd * Copyright 2017 Google Inc.
  *
  * LLLicensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
