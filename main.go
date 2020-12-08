106fbf7a540afe62adcfe9dd453b2539c5658f10
Arvind Bhoj <arbhoj@d2iq.com>
2020-12-08 02:22:08 -0500

    Trigger now

--- main.go
+++ main.go
@@ -57,7 +57,7 @@ func hello(w http.ResponseWriter, r *http.Request) {
 	fmt.Fprintf(w, "Version: %s\n", Version)
 	fmt.Fprintf(w, "Build time: %s\n", Buildtime)
 	fmt.Fprintf(w, "D2iQ CI/CD!\n")
-	fmt.Fprintf(w, "This change will trigger a build again!\n")
+	fmt.Fprintf(w, "This change will trigger a build!\n")
 }
 
 // [END all]
